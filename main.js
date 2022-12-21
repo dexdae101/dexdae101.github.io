164e7720f9a1038d93ee280ef1b82151da728ed9
dexdae101 <83088697+dexdae101@users.noreply.github.com>
2022-12-21 16:27:27 +0100

    added settings menu

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-let verbs = {"begginnen":"begonnen", "bleiben":"ist geblieben", "brechen":"gebrochen", "bringen":"gebracht", "denken":"gedacht", "duerfen":"gedurft", "essen":"gegessen", "finden":"gefunden", "fliegen":"geflogen", "geben":"gegeben", "gehen":"ist gegangen", "haben":"gehabt", "heissen":"istgeheissen", "kommen":"ist gekommen", "konnen":"gekonnt", "lesen":"gelesen", "moegen":"gemacht", "muessen":"gemusst", "nehmen":"genommen", "rufen":"gerufen", "schlafen":"geschlafen", "schreiben":"geschreiben","sehen":"gesehen", "sein":"ist gewesen", "singen":"gesungen", "sollen":"gesollt","sprechen":"gesprochen","stehen":"gestanden","treffen":"getroffen", "trinken":"getrunken","tun":"getan","waschen":"gewaschen", "wissen":"gewusst","wollen":"gewollt"}
+const verbs = {"begginnen":"begonnen", "bleiben":"ist geblieben", "brechen":"gebrochen", "bringen":"gebracht", "denken":"gedacht", "duerfen":"gedurft", "essen":"gegessen", "finden":"gefunden", "fliegen":"geflogen", "geben":"gegeben", "gehen":"ist gegangen", "haben":"gehabt", "heissen":"istgeheissen", "kommen":"ist gekommen", "konnen":"gekonnt", "lesen":"gelesen", "moegen":"gemacht", "muessen":"gemusst", "nehmen":"genommen", "rufen":"gerufen", "schlafen":"geschlafen", "schreiben":"geschreiben","sehen":"gesehen", "sein":"ist gewesen", "singen":"gesungen", "sollen":"gesollt","sprechen":"gesprochen","stehen":"gestanden","treffen":"getroffen", "trinken":"getrunken","tun":"getan","waschen":"gewaschen", "wissen":"gewusst","wollen":"gewollt"}
+const keys = Object.keys(verbs)
 
 //defining handy HTML DOM
+let game = document.getElementById("game")
 let out = document.getElementById("text")
 let txt = document.getElementById("input")
 txt.value = ""
@@ -8,13 +10,18 @@ let button = document.getElementById("btn")
 let widget = document.getElementById("widget")
 let countOut = document.getElementById("count")
 
-let keys = Object.keys(verbs)
+let imp = document.getElementById("imp")
+let imp_btn = document.getElementById("imp_btn")
+let mode_rad = document.getElementsByName("mode")
 
-let state = 0
+let state = false
 let count = 0
+let setting_state = false
+let mode = ""
 
+countOut.innerHTML = "0/" + keys.length
 
-function getRandom () {
+function play() {
        
     if (state) {
         if (txt.value.toLowerCase() == verbs[word]) {
@@ -40,7 +47,12 @@ function getRandom () {
         }
         txt.value = "" 
     } else {
-        state = 1
+        state = true
+        mode_rad.forEach(function (element) {
+            if (element.checked) {
+                mode = element.value
+            }
+        })
         btn.innerHTML = "conferma"
         word = keys[count]
         out.innerHTML = word
@@ -48,3 +60,22 @@ function getRandom () {
     
     
 };
+function stop() {
+    state = false
+    btn.innerHTML = "start!"
+    count = 0
+    countOut.innerHTML = "0/" + keys.length
+    out.innerHTML = ""
+}
+function settings() {
+    if (setting_state) {
+        setting_state = false
+        game.style.visibility = "visible"       
+        imp.style.visibility = "hidden"
+    } else {
+        setting_state = true
+        game.style.visibility = "hidden"       
+        imp.style.visibility = "visible"
+
+    }
+}
